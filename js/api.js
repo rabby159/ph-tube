2a184d4fd66084ddbe2de7a3fbcbc93146e919f8
Md Rabby <ablzerabbi159@gmail.com>
2023-09-01 22:56:55 +0600

    sorting complete

--- js/api.js
+++ js/api.js
@@ -24,20 +24,22 @@ const clickBlogBtn = () => {
     window.location.href = 'blog.html';
 }
 
+let contentData = [];
+
 
 const handleLoadData = async (id) =>{
     // console.log(id);
     const res = await fetch(`https://openapi.programming-hero.com/api/videos/category/${id}`)
     const data = await res.json();
-    console.log(data.data)
-
+    // console.log(data.data)
+    contentData = data.data;
 
     const dataContainer = document.getElementById('data-container');
     dataContainer.innerHTML = '';
 
-    if(data.data && data.data.length > 0){
+    if(contentData && contentData.length > 0){
        
-        data.data?.forEach(video => {
+        contentData?.forEach(video => {
             // console.log(video);
             // console.log(video.others.views)
             
@@ -103,6 +105,82 @@ const handleLoadData = async (id) =>{
     }
     
 };
+
+document.getElementById('btn-sort-by-view').addEventListener('click', () =>{
+    
+    contentData.sort((a, b) => parseFloat(b.others.views) - parseFloat(a.others.views));
+
+    const dataContainer = document.getElementById('data-container');
+    dataContainer.innerHTML = '';
+
+    if(contentData && contentData.length > 0){
+       
+        contentData?.forEach(video => {
+            // console.log(video);
+            // console.log(video.others.views)
+            
+            const seconds = `${video.others?.posted_date}`;
+            // console.log(seconds);
+
+                const timeConverting = (seconds) => {
+                    const hours = Math.floor(seconds / 3600);
+                    const remainingSeconds = seconds % 3600;
+                    const minutes = Math.floor(remainingSeconds / 60);
+    
+                return {
+                    hours: hours,
+                    minutes: minutes
+                  };
+                }
+    
+                const result = timeConverting(seconds);
+
+
+            const div = document.createElement('div');
+            div.classList = '';
+            div.innerHTML = `
+            <figure><img class='h-[200px] w-[500px]' src="${video?.thumbnail}" /></figure>
+                <div class='flex justify-end -mt-10'>
+                <button class=' bg-stone-700 text-white p-1 rounded'>${result.hours} hrs ${result.minutes} min ago</button>
+                </div>
+                <div class='flex items-center'>
+                    <div >
+                        <img class="w-16 h-16 px-2 rounded-full" src="${video?.authors[0]?.profile_picture}"/>
+                        
+                    </div>
+                    
+                
+                    <div class="card-body">
+                        <h2 class="card-title">${video?.title}</h2>
+                        <div class=''>
+                            <p class='inline-block'>${video?.authors[0]?.profile_name}</p>
+                            <img class='inline-block' src='${video.authors[0].verified? `./images/fi_10629607.svg` : '' }' />   
+                        </div> 
+                        <small>${video.others?.views}</small>
+                    </div>
+                </div>
+                
+            
+            `
+            dataContainer.appendChild(div);
+            
+           
+
+        });
+    }
+    else{
+        const noFoundData = document.createElement('div');
+        noFoundData.innerHTML = `
+        <div class='flex items-center gap-5'> 
+            <img src="./images/Icon.png"  />
+            <h2 class='text-xl font-semibold'>Oops!! Sorry, There is no content here</h2>
+        </div>
+        
+        `;
+        dataContainer.appendChild(noFoundData);
+    }
+
+});
 
 
 loadData();
